2e80ce75f8de81220a08547159c8f4a27976acd8
santiloureiro <santiloureiro12@gmail.com>
2025-10-07 21:47:25 -0300

    Añadir ejemplo de listas

--- clase-6/clase-6.py
+++ clase-6/clase-6.py
@@ -1,1 +1,13 @@
 # Hacer 3 ultimos ejercicios del TP 6
+
+#Imprimir lista por pantalla
+
+lista = [1,2,3,4,5]
+
+x = 0
+
+while x < 5:
+    print(lista[x], end=" ")
+    x = x + 1
+
+print("Fin de programa!")
